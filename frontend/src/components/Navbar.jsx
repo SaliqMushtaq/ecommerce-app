0d4002226f1501a58ebc3dbd905d611a8b72c685
SaliqMushtaq <mushtaqsaliq9@gmail.com>
2026-01-04 23:59:25 +0530

    Update Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   console.log('Navbar component rendered')
+  const  [visible , setVisible] = React.useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} className='w-36' alt="" />
@@ -19,9 +20,9 @@ const Navbar = () => {
 </div>
     <div className='group relative'>
       <img className='w-5' src={assets.profile_icon} alt="" />
-  <p></p>
-  <p></p>
-<p></p>
+  <p className='group-hover:block hidden absolute top-8 right-0 bg-gray-100 rounded-lg p-2'>Profile</p>
+  <p className='group-hover:block hidden absolute top-12 right-0 bg-gray-100 rounded-lg p-2'>Settings</p>
+<p className='group-hover:block hidden absolute top-16 right-0 bg-gray-100 rounded-lg p-2'>Logout</p>
     </div>
     <link href='/cart' className='relative'></link> 
     </div>
